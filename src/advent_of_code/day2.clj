6c135c032bc12bc868e59c1b18a809d83a2165ad
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-12-16 20:16:17 +0000

    Extract intcode for reuse.

--- src/advent_of_code/day2.clj
+++ src/advent_of_code/day2.clj
@@ -1,43 +1,15 @@
 (ns advent-of-code.day2
   (:require [clojure.string :as str]
-            [advent-of-code.util :as util]))
+            [advent-of-code.intcode :as intcode]))
 
 (def code (str/trim (slurp "resources/intcode.txt")))
 
-(declare do-op)
-
-(defn process-code
-  ([codes index]
-   (cond
-     (= (nth codes index) 1)
-     (do-op + codes index)
-     (= (nth codes index) 2)
-     (do-op * codes index)
-     (= (nth codes index) 99)
-     codes))
-  ([codes] (process-code codes 0)))
-
-(defn do-op [op codes index]
-  (let [first-op (nth codes (nth codes (+ 1 index)))
-        second-op (nth codes (nth codes (+ 2 index)))
-        result (op first-op second-op)]
-    (process-code (util/replace-nth codes (nth codes (+ 3 index)) result) (+ 4 index))))
-
-(defn process [sv noun verb]
-  (let [s (into [] (map #(Integer/parseInt %) (str/split sv  #",")))
-        c1 (util/replace-nth s 1 noun)
-        c2 (util/replace-nth c1 2 verb)]
-    (process-code c2)))
-
-(defn print-result [noun verb]
-  (println noun verb (+ (* 100 noun) verb)))
-
 (defn solve-part1 []
-  (process code 12 2))
+  (intcode/process code 12 2))
 
 (defn solve-part2 []
   (for [verb (range 99)
         noun (range 99)
-        :let [output (first (process code noun verb))]
+        :let [output (first (intcode/process code noun verb))]
         :when (= output 19690720)]
-    (print-result noun verb)))
+    (intcode/print-result noun verb)))
